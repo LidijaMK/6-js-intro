3a5fa6aa72da3a3b6f6b99009eb70bb8ea178fc3
LidijaMK <79445808+LidijaMK@users.noreply.github.com>
2021-03-16 15:54:38 +0200

    marks update

--- js/12-marks.js
+++ js/12-marks.js
@@ -1,34 +1,31 @@
-function pazymiuVidurkis(name, marks) {
-  let suma = 0;
+function pazymiuVidurkis(vardas, pazymiai) {
+    let suma = 0;
 
-  for (let i = 0; i < pazymiai.length; i++) {
-      suma += pazymiai[i];
-  }
-  const average = suma / pazymiai.length;
+    for (let i = 0; i < pazymiai.length; i++) {
+        suma += pazymiai[i];
+    }
+
+    const average = suma / pazymiai.length;
 
     // Vardenis: pazymiu vidurkis yra 6.
-    console.log(`${vardas}: pazymiu vidurkis yra ${average}.`)
+    console.log(`${vardas}: pazymiu vidurkis yra ${average}.`);
 }
 
-//pazymiuVidurkis('Petriukas', [10, 2, 8, 4]);
-//pazymiuVidurkis('Maryte', [10, 8, 6, 4]);
-//pazymiuVidurkis('Alphe', [5, 10, 8, 7, 6]);
-
-console.log('--------------');
+// pazymiuVidurkis('Petriukas', [10, 2, 8, 4]);
+// pazymiuVidurkis('Maryte', [10, 8, 6, 4]);
+// pazymiuVidurkis('Alphe', [10, 9, 10, 7, 9, 2, 7]);
 
 const mokykla = [
     ['Petriukas', [10, 2, 8, 4]],
     ['Maryte', [10, 8, 6, 4]],
-    ['Alphe', [5, 10, 8, 7, 6]]
+    ['Alphe', [10, 9, 10, 7, 9, 2, 7]]
 ];
 
-console.log(mokykla);
-
-for (let i=0; i<mokykla.length; i++) {
-    console.log('--------------');
+for (let i = 0; i < mokykla.length; i++) {
     const mokinys = mokykla[i];
-    const name = (mokykla[0]);
-    const marks = (mokykla[1]);
-}
+    const name = mokinys[0];
+    const marks = mokinys[1];
 
-pazymiuVidurkis(name, marks)
+    pazymiuVidurkis(name, marks);
+    pazymiuVidurkis(mokykla[i][0], mokykla[i][1]);
+}
