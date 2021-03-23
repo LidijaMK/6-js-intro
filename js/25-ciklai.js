e097b2851954717d1aea640ec46c6adbd67ccf6b
LidijaMK <79445808+LidijaMK@users.noreply.github.com>
2021-03-23 17:38:42 +0200

    ciklai

--- js/25-ciklai.js
+++ js/25-ciklai.js
@@ -125,6 +125,180 @@ do {
 
 
 
+console.clear();
+
+// FOREACH
+// jo sustabdyti yra neimanoma
+// leta funkcija 
+
+//list.forEach(function(){                            //anonimine funkcija
+//    console.log('labas');
+//})
+/*
+list.forEach(function (item, i){                            
+    console.log(i, item);
+})  
+*/
+
+// supaprastinta
+list.forEach(item => console.log(item));
+
+let FEsuma = 0;
+
+list.forEach(n => FEsuma += n);             
+console.log(FEsuma);
+
+console.log('------------------');
+
+// uzdavinukas, kai sumuojame arejus arejuje
+/*
+const doubleList = [[1, 5,], [8, 99], [-8, -7, 15, 4], [3]];
+let dlSuma = 0;
+doubleList.forEach(innerList => innerList.forEach(n => dlSuma += n));
+console.log(dlSuma);
+*/
+
+//uzdavinukas, kai liste turime ne tik arejus, bet ir skaicius
+const doubleList = [[1, 5,], 15, [8, 99], -49, [-8, -7, 15, 4], [3]];
+let dlSuma = 0;
+//doubleList.forEach(innerItem => innerItem.forEach(n => dlSuma += n));
+
+doubleList.forEach(innerItem => typeof innerItem === 'number' 
+    ? dlSuma += innerItem
+    : innerItem.forEach(n => dlSuma += n)
+);
+console.log(dlSuma);
+
+// MAP            (dazniausiai naudojamas, kai norime modifikuoti duomenis, skirtas darbui su sarasais; mapas neveikia ant stringu (zodiai) ir objektu)
+// map() metodas perejas per duota sarasa grazina tokio pacio dydzio sarasa, tik su pakeistomis reiksmemis
+// su mapu turime ir originalu sarasa ir modifikuota sarasa
+
+
+const mapList = [1, 2, 3, 4, 5, 6];
+const forListSquare = [];
+
+for (const num of mapList) {
+    forListSquare.push(num * num);
+} 
+console.log(forListSquare);
+
+/*******************************/
+
+const mapListSquare = mapList.map(n => n * n);
+console.log(mapListSquare);
+
+/***********************************/
+
+console.clear();
+
+
+// duomenu taisymo uzdavinys: visi neigiami skaiciai turi buti konvertuoti i nuli
+
+const pazymiai = [10, -8, 2, -6, 8, -2];
+const konvertuotiPazymiai = pazymiai.map(n => n < 0 ? 0: n);
+console.log(pazymiai);
+console.log(konvertuotiPazymiai);
+
+
+
+/***********************************/
+
+console.clear();
+
+// reikia grazinti sveikuosius skaicius
+
+const desimtainiai = [3.14, 5, 6.7, -14.7, -8, 0.001];
+
+const sveikieji = desimtainiai.map(n => Math.round(n));
+console.log(desimtainiai);
+console.log(sveikieji);
+
+/************************************/
+
+// cia modifikuojame originalius duomenis (pavyzdys kaip galima parasyti su paprastuoju ciklu):
+for (let i = 0; i < desimtainiai.length; i++) {
+    desimtainiai[i] = Math.round(desimtainiai[i]);
+}
+console.log(desimtainiai);
+
+// arrejus ir objektas yra kompleksiniai kintamieji; skaiciai, stringai yra primityvus kintamieji 
+
+
+/***********************************/
+
+console.clear();
+
+
+// FILTER            
+
+// uzdavinys: atrinkti tik tikrus pazymius, t.y. [1..10]
+
+
+const marks = [10, -8, 2, 3.14, -6, 7.5, 8, -2, 11, 55];
+const filteredMarks = marks.filter(m => m >= 1 && m <= 10 && m % 1 === 0);
+
+console.log(marks);
+console.log(filteredMarks);
+
+
+console.clear();
+
+
+// palikti zodzius is didziosiso raides
+
+const dictionary = ['Labas', 'rytas', 'Lietuva', 'sakau', 'tau'];
+
+const isDidziosios = dictionary.filter(word => word[0] === word[0].toUpperCase());
+
+console.log(dictionary);
+console.log(isDidziosios);
+
+console.clear();
+
+// REDUCE
+
+// eina per sarasa ir paima visas reiksmes is eiles ir su jomis padaro viena bendra rezultata
+// pvz.: jei turime pazymiu sarasa, tai eina per pazymius ir suskaiciuoja pazymiu suma
+
+
+const petriukoPazymiai = [10, 2, 8, 6, 4];
+
+//I var. su forEach;
+let petriukoPazymiuSuma = 20;
+petriukoPazymiai.forEach(p => petriukoPazymiuSuma += p);
+console.log(petriukoPazymiuSuma);
+
+const reducePazymiai = petriukoPazymiai.reduce((total, paz) => total + paz, 20);
+console.log(reducePazymiai);
+ 
+// 20 - initial value (nuo kurios sumos reikia pradeti sumuoti)
+
+
+//koks gausis tekstas, jei sujungsime visu zodziu raides
+const dictionary2 = ['Labas', 'rytas', 'Lietuva', 'sakau', 'tau'];
+
+const short = dictionary2.reduce((total, word) => total + word[0], '');
+console.log(short);
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
