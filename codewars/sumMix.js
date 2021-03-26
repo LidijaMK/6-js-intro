84044f8704727fe1fe6bdac49840b2e97b030d54
LidijaMK <79445808+LidijaMK@users.noreply.github.com>
2021-03-26 20:45:18 +0200

    sumMix

---
+++ codewars/sumMix.js
@@ -0,0 +1,25 @@
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+https://www.codewars.com/kata/57eaeb9578748ff92a000009/train/javascript
+
+*/
+
+function sumMix(x){
+    let sum = 0   
+    for (let i = 0; i < x.length; i++) {   
+        sum += x[i] * 1;
+    }
+    return sum;
+}
+
+    //return x.map(a => +a).reduce((a, b) => a + b);
+
+    console.log(sumMix([9, 3, '7', '3']), '->', 22);
+    console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), '->', 42); 
+    console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), '->', 41);
+
+
+
